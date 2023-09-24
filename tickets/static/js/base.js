6e0008a356e45412f2dc18e13332285927932122
Israel Colín <israaacolin@gmail.com>
2023-09-23 23:23:12 -0600

    Haciendo que el dropdown se despliegue al pulsar

--- tickets/static/js/base.js
+++ tickets/static/js/base.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    // Desplegar el dropdown cuando el puntero esta encima
-    $('.ui.dropdown').dropdown({
-        on: 'hover', 
-    });
+    // Desplegar el dropdown
+    $('.ui.dropdown').dropdown();
 })
