fd3bd6aa20684cd602664f3df41b6aac9b56735a
Israel Colín <israaacolin@gmail.com>
2023-09-24 14:18:50 -0600

    Unificando las vistas de crear y editar

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='Index'),
     path('categoría', views.categoría, name='Categoría'),
-    path('categoría/editar-eliminar/<int:id>', views.editar_eliminar_categoría, name='Editar/Eliminar categoría'),
+    path('categoría/editar-eliminar/<int:id>', views.categoría, name='Editar/Eliminar categoría'),
     path('prioridad', views.prioridad, name='Prioridad'),
-    path('prioridad/editar-eliminar/<int:id>', views.editar_eliminar_prioridad, name='Editar/Eliminar prioridad'),
+    path('prioridad/editar-eliminar/<int:id>', views.prioridad, name='Editar/Eliminar prioridad'),
     path('subcategoría', views.subcategoría, name='Subcategoría'),
-    path('subcategoría/editar-eliminar/<int:id>', views.editar_eliminar_subcategoría, name='Editar/Eliminar subcategoría'),
+    path('subcategoría/editar-eliminar/<int:id>', views.subcategoría, name='Editar/Eliminar subcategoría'),
     path('cargar_subcategorias', views.cargar_subcategorías, name='Cargar subcategoría'),
     path('crear_ticket', views.crear_ticket, name='Crear ticket'),
 ]
