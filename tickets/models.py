e16d4121a45a7091a3ac42fe932238afdbed52d9
Israel Colín <israaacolin@gmail.com>
2023-10-08 11:37:13 -0600

    Organizando models.py

--- tickets/models.py
+++ tickets/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class UsuarioManager(BaseUserManager):
+
     def create_user(self, nombres_usuario, appat_usuario, apmat_usuario, num_empleado, num_tel, email, rol = None, password = None):
+
         if not email:
             raise ValueError('El usuario debe tener un correo electrónico')
         
@@ -20,9 +22,11 @@ class UsuarioManager(BaseUserManager):
 
         usuario.set_password(password)
         usuario.save()
+
         return usuario
     
     def create_superuser(self, nombres_usuario, appat_usuario, apmat_usuario, num_empleado, num_tel, email, password):
+
         usuario = self.create_user(
             nombres_usuario = nombres_usuario,
             appat_usuario = appat_usuario,
@@ -36,14 +40,17 @@ class UsuarioManager(BaseUserManager):
 
         usuario.usuario_administrador = True
         usuario.save()
+
         return usuario
 
 
 class Usuario(AbstractBaseUser):
+
     OPCIONES_ROL = (
         (2, 'Soporte'),
         (3, 'Usuario'),
     )
+
     nombres_usuario = models.CharField('Nombre(s)', max_length=200)
     appat_usuario = models.CharField('Apellido paterno', max_length=50)
     apmat_usuario = models.CharField('Apellido materno', max_length=50)
@@ -73,14 +80,16 @@ class Usuario(AbstractBaseUser):
     
 
 class Categoría(models.Model):
+
     id_categoría = models.AutoField(verbose_name='Id categoría', primary_key=True)
     nombre_cat = models.CharField(verbose_name='Nombre de categoría', max_length=50)
 
-    def __str__(self) -> str:
+    def __str__(self) -> str:        
         return self.nombre_cat
 
 
 class SubCategoría(models.Model):
+
     id_subcategoría = models.AutoField(verbose_name='Id subcategoría', primary_key=True)
     id_categoría = models.ForeignKey(Categoría, on_delete=models.CASCADE)
     nombre_subCat = models.CharField(verbose_name='Nombre de subcategoría', max_length=50)
@@ -98,12 +107,14 @@ class Prioridad(models.Model):
 
 
 class Ticket(models.Model):
+
     ESTADO_CHOICES = (
         (1, 'Abierto'),
         (2, 'En Progreso'),
         (3, 'Completado'),
         (4, 'Pausado'),
     )
+
     usuario_creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     id_ticket = models.AutoField(verbose_name='Id de ticket', primary_key=True)
     id_categoría = models.ForeignKey(Categoría, on_delete=models.CASCADE, verbose_name='Categoría')
@@ -117,7 +128,9 @@ class Ticket(models.Model):
     estado = models.IntegerField(verbose_name='Estado', choices=ESTADO_CHOICES, default=1)
 
     def cerrar_tarea(self):
+
         if self.estado != 3:
+            
             self.estado = 3
             self.fecha_fin = timezone.now()
             self.duracion = self.fecha_fin - self.fecha_inicio  # Calcula la duración
